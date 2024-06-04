4b51e0191c82a8c58ab697d9329bfae63d422ec1
vadill0 <aitorvadillo70@gmail.com>
2024-06-05 00:52:12 +0200

    unnecessary imports removed

--- app/src/main/java/com/example/mangapp/ForgotPasswordFragment.java
+++ app/src/main/java/com/example/mangapp/ForgotPasswordFragment.java
@@ -2,7 +2,6 @@ package com.example.mangapp;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 import android.util.Log;
@@ -13,8 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
