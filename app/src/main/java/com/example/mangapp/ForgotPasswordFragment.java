cdfac3071622ffc54e2911010bf7e2d6883fdd81
vadill0 <aitorvadillo70@gmail.com>
2024-06-05 00:23:04 +0200

    recovery email functionality added

--- app/src/main/java/com/example/mangapp/ForgotPasswordFragment.java
+++ app/src/main/java/com/example/mangapp/ForgotPasswordFragment.java
@@ -2,21 +2,50 @@ package com.example.mangapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
 
 public class ForgotPasswordFragment extends DialogFragment {
 
+    private static final String TAG = "PasswordRecovery";
+    private FirebaseAuth mAuth;
+    EditText editTextEmailRecovery;
+    Button buttonRecovery;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false);
+        View view = inflater.inflate(R.layout.fragment_forgot_password, container, false);
+
+        editTextEmailRecovery = view.findViewById(R.id.editTextEmailRecovery);
+        buttonRecovery = view.findViewById(R.id.buttonRecovery);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        buttonRecovery.setOnClickListener((View v) ->{
+            String email = editTextEmailRecovery.getText().toString();
+
+            if(SignInActivity.emailValidation(email)){
+                Toast.makeText(getActivity(),"Invalid email",Toast.LENGTH_SHORT).show();
+            }else{
+                sendRecoveryEMail(email);
+            }
+        });
+
+        return view;
     }
 
     @Override
@@ -29,5 +58,13 @@ public class ForgotPasswordFragment extends DialogFragment {
             int height = getResources().getDimensionPixelSize(R.dimen.FPdialog_height);
             Objects.requireNonNull(getDialog().getWindow()).setLayout(width, height);
         }
+    }
+
+    public void sendRecoveryEMail(String email){
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                Log.d(TAG,"Email sent");
+            }
+        });
     }
 }
