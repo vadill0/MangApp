4f34966bc9888c8b8391ea09b2ce40054858202f
vadill0 <aitorvadillo70@gmail.com>
2024-06-07 19:27:41 +0200

    verification fragment added && GUI done

---
+++ app/src/main/java/com/example/mangapp/EmailVerificationFragment.java
@@ -0,0 +1,19 @@
+package com.example.mangapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EmailVerificationFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_email_verification, container, false);
+    }
+}
