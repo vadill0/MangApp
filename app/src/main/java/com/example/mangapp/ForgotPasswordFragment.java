ab3161028dbaf812f8d5b139705b5d7e21d365c0
vadill0 <aitorvadillo70@gmail.com>
2024-06-07 17:50:10 +0200

    back button implemented in ForgotPasswordFragment.java

--- app/src/main/java/com/example/mangapp/ForgotPasswordFragment.java
+++ app/src/main/java/com/example/mangapp/ForgotPasswordFragment.java
@@ -2,7 +2,9 @@ package com.example.mangapp;
 
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
@@ -20,6 +23,7 @@ public class ForgotPasswordFragment extends Fragment {
     private static final String TAG = "PasswordRecovery";
     private FirebaseAuth mAuth;
     EditText editTextEmailRecovery;
+    ImageView imageViewReturn;
     Button buttonRecovery;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,6 +32,7 @@ public class ForgotPasswordFragment extends Fragment {
 
         editTextEmailRecovery = view.findViewById(R.id.editTextEmailRecovery);
         buttonRecovery = view.findViewById(R.id.buttonRecovery);
+        imageViewReturn = view.findViewById(R.id.imageViewReturn);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -38,6 +43,28 @@ public class ForgotPasswordFragment extends Fragment {
                 Toast.makeText(getActivity(),"Invalid email",Toast.LENGTH_SHORT).show();
             }else{
                 sendRecoveryEMail(email);
+            }
+        });
+
+        imageViewReturn.setOnClickListener((View v) ->{
+            if(getActivity() != null){
+                getActivity().getOnBackPressedDispatcher().onBackPressed();
+            }
+        });
+
+        // Funcion para ir para atras y rellenar el activity
+        requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (getActivity() != null) {
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    if (fragmentManager.getBackStackEntryCount() > 1) {
+                        fragmentManager.popBackStack();
+                    } else {
+                        setEnabled(false);
+                        getActivity().getOnBackPressedDispatcher().onBackPressed();
+                    }
+                }
             }
         });
 
