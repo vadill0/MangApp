87a97574fd190666fca1cac189baa8572cae532f
vadill0 <aitorvadillo70@gmail.com>
2024-06-10 23:37:53 +0200

    MangaFragment GUI started

---
+++ app/src/main/java/com/example/mangapp/MangaFragment.java
@@ -0,0 +1,23 @@
+package com.example.mangapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class MangaFragment extends Fragment {
+
+    ImageView imageViewReturn;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_manga, container, false);
+
+        return view;
+    }
+}
